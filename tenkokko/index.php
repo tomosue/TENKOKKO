6f3ccc1edd42fd2fb67db0064616b2265d6de4e2
tomosue <toshimasatomosue@603bzhbzz.local>
2019-04-14 17:23:40 +0900

    修正

--- tenkokko/index.php
+++ tenkokko/index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "Hello World!";
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+//POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input');
+error_log
 
 ?>
