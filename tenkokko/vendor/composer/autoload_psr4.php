c467f82701b0b19ebbb9feb1c0dba8b3f7e6bf15
tomosue <toshimasatomosue@603bzhbzz.local>
2019-04-22 22:15:30 +0900

    修正

--- tenkokko/vendor/composer/autoload_psr4.php
+++ tenkokko/vendor/composer/autoload_psr4.php
@@ -6,5 +6,15 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Symfony\\Component\\DomCrawler\\' => array($vendorDir . '/symfony/dom-crawler'),
+    'Symfony\\Component\\CssSelector\\' => array($vendorDir . '/symfony/css-selector'),
+    'Symfony\\Component\\BrowserKit\\' => array($vendorDir . '/symfony/browser-kit'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'LINE\\' => array($vendorDir . '/linecorp/line-bot-sdk/src'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Goutte\\' => array($vendorDir . '/fabpot/goutte/Goutte'),
 );
--- tenkokko/vendor/composer/autoload_real.php
+++ tenkokko/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit7b0da76f245287f8ccf802c079e92296
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit7b0da76f245287f8ccf802c079e92296::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire7b0da76f245287f8ccf802c079e92296($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire7b0da76f245287f8ccf802c079e92296($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
