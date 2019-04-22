496849d31db21285cdaca68a4e747242d26382d3
tomosue <toshimasatomosue@603bzhbzz.local>
2019-04-22 22:05:08 +0900

    修正

--- tenkokko/index.php
+++ tenkokko/index.php
@@ -5,16 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-
-// LINE Messaging APIがリクエストに付与した著名を取得
+// LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-//著名が正しいかチェック。正しければリクエストをパースし配列へ                            
-$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
-
 
 //ループ            
 foreach ($events as $event){
