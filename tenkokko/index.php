2fe25f0bcbaf12477647830c630296f5f9d79229
tomosue <toshimasatomosue@603bzhbzz.local>
2019-04-22 11:40:44 +0900

    修正

--- tenkokko/index.php
+++ tenkokko/index.php
@@ -4,20 +4,16 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
-$httpClient = new\LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
-                            ('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new\LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-$bot = new\LINE\LINEBot($httpClient, ['channelSecret' => getenv(
-                            'CHANNEL_SECRET')]);
+$bot = new\LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 // LINE Messaging APIがリクエストに付与した著名を取得
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
-                            ::LINE_SIGNATURE];
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 //著名が正しいかチェック。正しければリクエストをパースし配列へ                            
-$events = $bot->parseEventRequest(file_get_contents('php://input'),
-                            $signature);
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
             
 foreach ($events as $event){
